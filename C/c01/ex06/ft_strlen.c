7fd042f34cbd25317254b032ab281740e4616e48
usantos- <usantos-@student.42sp.org.br>
2021-04-06 23:22:34 +0000

    Renamed folder name.

--- C/c01/ex06/ft_strlen.c
+++ C/c01/ex06/ft_strlen.c
@@ -1,44 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: usantos- <usantos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/03 06:24:29 by usantos-          #+#    #+#             */
-/*   Updated: 2021/04/04 18:08:18 by usantos-         ###   ########.fr       */
+/*   Created: 2021/04/06 03:25:11 by usantos-          #+#    #+#             */
+/*   Updated: 2021/04/06 20:32:11 by usantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(int n)
+int	ft_strlen(char *str)
 {
-	write(1, &n, 1);
-}
+	int	i;
 
-void	ft_print_comb2(void)
-{
-	int	x;
-	int y;
-
-	x = 0;
-	while (x <= 99)
+	i = 0;
+	while (*str)
 	{
-		y = x + 1;
-		while (y <= 99)
-		{
-			ft_putchar('0' + (x / 10));
-			ft_putchar('0' + (x % 10));
-			write(1, " ", 1);
-			ft_putchar('0' + (y / 10));
-			ft_putchar('0' + (y % 10));
-			if (x != 98 || y != 99)
-			{
-				write(1, ", ", 2);
-			}
-			y++;
-		}
-		x++;
+		i++;
+		str++;
 	}
+	return (i);
 }
