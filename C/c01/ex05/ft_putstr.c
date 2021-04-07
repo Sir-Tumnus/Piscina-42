53c13a1a1bbef2c500c2965d109f4695540f23dc
usantos- <usantos-@student.42sp.org.br>
2021-04-07 04:39:05 +0000

    Update function on C01 - ex05.

--- C/c01/ex05/ft_putstr.c
+++ C/c01/ex05/ft_putstr.c
@@ -6,26 +6,14 @@
 /*   By: usantos- <usantos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 01:56:34 by usantos-          #+#    #+#             */
-/*   Updated: 2021/04/06 19:08:02 by usantos-         ###   ########.fr       */
+/*   Updated: 2021/04/07 04:36:13 by usantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_putstr(char *str)
 {
-	char	print_char;
-
-	print_char = '0';
-	while (print_char != '\0')
-	{
-		print_char = *str;
-		ft_putchar(print_char);
-		str++;
-	}
+	while (*str)
+		write(1, str++, 1);
 }
